0c923f10dc21a04b557c0fde2c4677d0dfe50d68
trchm4n <kntkntkntkntknt@gmail.com>
2026-07-07 02:18:40 +0900

    Update enemy.js

--- js/entities/enemy.js
+++ js/entities/enemy.js
@@ -11,25 +11,20 @@ export default class Enemy {
         this.speed = 120 + Math.random() * 120;
 
         this.alive = true;
-
     }
 
     update(delta) {
 
         this.y += this.speed * delta;
 
-        // 画面外で削除
         if (this.y > window.innerHeight + 50) {
             this.alive = false;
         }
-
     }
 
     draw(ctx) {
 
         ctx.fillStyle = "#ff4d4d";
-
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-
 }
