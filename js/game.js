6ec7170940ec7bfcfad47700d63e0201cd410f6f
trchm4n <kntkntkntkntknt@gmail.com>
2026-07-06 23:51:40 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,68 @@
+class Game {
 
+    constructor() {
+
+        this.titleScreen =
+            document.getElementById("titleScreen");
+
+        this.gameContainer =
+            document.getElementById("gameContainer");
+
+        this.gameOver =
+            document.getElementById("gameOver");
+
+        this.startButton =
+            document.getElementById("startButton");
+
+        this.retryButton =
+            document.getElementById("retryButton");
+
+        this.titleButton =
+            document.getElementById("titleButton");
+
+        this.startButton.addEventListener(
+            "click",
+            () => this.start()
+        );
+
+        this.retryButton.addEventListener(
+            "click",
+            () => this.start()
+        );
+
+        this.titleButton.addEventListener(
+            "click",
+            () => this.backToTitle()
+        );
+
+    }
+
+    start() {
+
+        this.titleScreen.classList.add("hidden");
+
+        this.gameOver.classList.add("hidden");
+
+        this.gameContainer.classList.remove("hidden");
+
+        console.log("GAME START");
+
+    }
+
+    backToTitle() {
+
+        this.gameContainer.classList.add("hidden");
+
+        this.gameOver.classList.add("hidden");
+
+        this.titleScreen.classList.remove("hidden");
+
+    }
+
+}
+
+window.onload = () => {
+
+    new Game();
+
+};
